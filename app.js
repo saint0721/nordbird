03872a76eff6ab93089a883930779a4196bf73b3
Saint <sikim0721@gmail.com>
2024-03-21 11:30:23 +0900

    완성.. 대신 스스로 해보기를 더 연습해야겠다.
    목록
    1. 팔로잉 끊기
    프로필 정보 변경하기
    게시글 좋아요 누르기
    게시글 삭제하기
    매번 데이터베이스를 조회하지 않도록 deserializeUser 캐싱하기

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const path = require('path')
 const session = require('express-session')
 const nunjucks = require('nunjucks')
 const dotenv = require('dotenv')
-dotenv.config()
+const passport = require('passport')
 
+dotenv.config()
 const pageRouter = require('./routes/page')
 const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
+const userRouter = require('./routes/user')
 const { sequelize } = require('./models')
 const passportConfig = require('./passport')
-const passport = require('passport')
 
 const app = express()
 passportConfig()
@@ -31,7 +33,9 @@ sequelize.sync({ force: false })
 
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/img', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(session({
   resave: false,
@@ -47,6 +51,8 @@ app.use(passport.session())
 
 app.use('/', pageRouter)
 app.use('/auth', authRouter)
+app.use('/post', postRouter)
+app.use('/user', userRouter)
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
